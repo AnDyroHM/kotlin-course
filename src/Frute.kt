6a21cd7f91e98253aec311d0c7e2755305f54026
Angel Mendez <angelmendezz751@gmail.com>
2018-03-19 12:45:23 -0600

    Create my first class

---
+++ src/Frute.kt
@@ -0,0 +1,15 @@
+class Frute(color:String, sabor:String, precio:Int){
+
+    var color:String = ""
+    var sabor:String = ""
+    var precio:Int = 0
+    init {
+        this.color = color
+        this.sabor = sabor
+        this.precio = precio
+    }
+}
+
+fun main(args: Array<String>){
+    println("Hello world")
+}
