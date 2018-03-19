d3fd16fef3466fc6f32cb76c542347184609703c
Angel Mendez <angelmendezz751@gmail.com>
2018-03-19 13:03:30 -0600

    Create pudrirse method

--- src/Frute.kt
+++ src/Frute.kt
@@ -3,14 +3,31 @@ class Frute(color:String, sabor:String, precio:Int){
     var color:String = ""
     var sabor:String = ""
     var precio:Int = 0
+    var frescura:Int = 100
+    var pudrirse:Int = 0
     init {
         this.color = color
         this.sabor = sabor
         this.precio = precio
+        this.frescura = 100
+    }
+
+    fun pudrirse(cant: Int = pudrirse){
+        this.frescura -= cant
+    }
+
+    fun showStatus(){
+        val frescura = this.frescura
+        if (frescura <= 0){
+            println("Tu fruta se ha podrido")
+        }else{
+            println("Fruta con frescura al $frescura%")
+        }
     }
 }
 
 fun main(args: Array<String>){
-    var manzana  = Frute(color = "Rojo",sabor = "Dulce",precio = 4);
-    println(manzana.color)
+    val manzana  = Frute(color = "Rojo",sabor = "Dulce",precio = 4)
+    manzana.pudrirse(cant = 110)
+    manzana.showStatus()
 }
