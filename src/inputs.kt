9a622b05b78512b99711784d882290abf2d901a1
Angel Mendez <angelmendezz751@gmail.com>
2018-03-23 18:34:46 -0600

    Input inits

---
+++ src/inputs.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>){
+    var name = readLine()
+    var age = readLine()
+    var dep:String?
+    dep = "Software engineering"
+    println("name: ${name}")
+    println("age: ${age}")
+}
